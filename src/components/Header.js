4f1e5c3cc1c738e27c2c35d8cc326d45eb27ead1
Michael Vasyukov <michael.vasukovff@gmail.com>
2021-04-03 19:16:59 +0300

    project it-kamasutra start

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://place-hold.it/75" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,28 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://place-hold.it/900x100" />
+            </div>
+            <div>
+                ava + desc
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
