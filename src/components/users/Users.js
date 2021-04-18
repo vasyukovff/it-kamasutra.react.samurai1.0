83d6ade0af4dfdd31f5c14780359c442acc054cc
Michael Vasyukov <michael.vasukovff@gmail.com>
2021-04-18 20:38:28 +0300

    refactor

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -4,22 +4,7 @@ import UserItem from './UserItem';
 
 let Users = (props) => {
 
-    // if (props.users.length === 0) {
-    //     props.setUsers([
-    //         { id: 1, photoUrl: 'https://place-hold.it/50', followed: false, fullName: 'Dmitry', status: 'iam a boss', location: { city: 'Minsk', country: 'Belarus' } },
-    //         { id: 2, photoUrl: 'https://place-hold.it/50', followed: true, fullName: 'Sasha', status: 'status ', location: { city: 'City Green', country: 'Gerany' } },
-    //         { id: 3, photoUrl: 'https://place-hold.it/50', followed: false, fullName: 'Misha 2', status: 'haha a am .net developer', location: { city: 'Los Angeles', country: 'UA' } },
-    //         { id: 4, photoUrl: 'https://place-hold.it/50', followed: false, fullName: 'Alexandr 3', status: 'software engeener', location: { city: 'Moscow', country: 'Russia' } }
-    //     ])
-    // }
-
     const addUser = () => {
-        // props.setUsers([
-        //     { id: 1, photoUrl: 'https://place-hold.it/50', followed: false, fullName: 'Dmitry', status: 'iam a boss', location: { city: 'Minsk', country: 'Belarus' } },
-        //     { id: 2, photoUrl: 'https://place-hold.it/50', followed: true, fullName: 'Sasha', status: 'status ', location: { city: 'City Green', country: 'Gerany' } },
-        //     { id: 3, photoUrl: 'https://place-hold.it/50', followed: false, fullName: 'Misha 2', status: 'haha a am .net developer', location: { city: 'Los Angeles', country: 'UA' } },
-        //     { id: 4, photoUrl: 'https://place-hold.it/50', followed: false, fullName: 'Alexandr 3', status: 'software engeener', location: { city: 'Moscow', country: 'Russia' } }
-        // ])
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
             props.setUsers(response.data.items);
         })
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,12 +3,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET-USERS';
 
 let initialState = {
-    users: [
-        /*{ id: 1, photoUrl: 'https://place-hold.it/50', followed: false, fullName: 'Dmitry', status: 'iam a boss', location: { city: 'Minsk', country: 'Belarus' } },
-        { id: 2, photoUrl: 'https://place-hold.it/50', followed: true, fullName: 'Sasha', status: 'status ', location: { city: 'City Green', country: 'Gerany' } },
-        { id: 3, photoUrl: 'https://place-hold.it/50', followed: false, fullName: 'Misha 2', status: 'haha a am .net developer', location: { city: 'Los Angeles', country: 'UA' } },
-        { id: 4, photoUrl: 'https://place-hold.it/50', followed: false, fullName: 'Alexandr 3', status: 'software engeener', location: { city: 'Moscow', country: 'Russia' } }*/
-    ]
+    users: []
 };
 
 const usersReducer = (state = initialState, action) => {
