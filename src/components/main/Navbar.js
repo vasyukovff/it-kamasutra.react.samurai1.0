57ed2827c98858326d3ee59cd44635397edc3106
Michael Vasyukov <michael.vasukovff@gmail.com>
2021-04-19 19:38:10 +0300

    #53 classes component, extands React.Component

--- src/components/main/Navbar.js
+++ src/components/main/Navbar.js
@@ -15,6 +15,11 @@ const Navbar = () => {
                 </NavLink>
             </div>
             <div className={classes.item}>
+                <NavLink to="/users" activeClassName={classes.active} >
+                    Users
+                </NavLink>
+            </div>
+            <div className={classes.item}>
                 <NavLink to="/news" activeClassName={classes.active} >
                     News
                 </NavLink>
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,24 +2,27 @@ import axios from 'axios';
 import React from 'react';
 import UserItem from './UserItem';
 
-let Users = (props) => {
+class Users extends React.Component {
 
-    const addUser = () => {
+    constructor(props) {
+        super(props);
+
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
+            this.props.setUsers(response.data.items);
         })
     }
 
-    return (
-        <>
-            <div>
-                {
-                    props.users.map(user => { return <UserItem {...user} onFollow={props.onFollow} onUnfollow={props.onUnfollow} key={user.id} /> })
-                }
-            </div>
-            <button onClick={addUser}>AddUsers</button>
-        </>
-    )
+    render() {
+        return (
+            <>
+                <div>
+                    {
+                        this.props.users.map(user => { return <UserItem {...user} onFollow={this.props.onFollow} onUnfollow={this.props.onUnfollow} key={user.id} /> })
+                    }
+                </div>
+            </>
+        )
+    }
 }
 
 export default Users;
