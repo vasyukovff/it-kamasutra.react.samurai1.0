7e7ba592de81ede638cde4f44fd48b4b51195f78
Michael Vasyukov <michael.vasukovff@gmail.com>
2021-04-27 04:24:08 +0300

    Refactor userAPI, axios.create, use instance

--- src/api/api.js
+++ src/api/api.js
@@ -1,31 +1,25 @@
 import axios from "axios"
 
-const baseUrl = 'https://social-network.samuraijs.com/api/1.0/';
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "8cc572da-38e6-49ab-a5f6-ce1b84a913a5"
+    }
+});
 
 export const getUsers = (pageSize, page) => {
-    return axios.get(baseUrl + `users?count=${pageSize}&page=${page}`, { withCredentials: true }).then(response => response.data);
+    return instance.get(`users?count=${pageSize}&page=${page}`).then(response => response.data);
 }
 
 export const unfollow = (userId) => {
-    return axios.delete(baseUrl + `follow/${userId}`,
-        {
-            withCredentials: true,
-            headers: {
-                "API-KEY": "8cc572da-38e6-49ab-a5f6-ce1b84a913a5"
-            }
-        }).then(response => response.data);
+    return instance.delete(`follow/${userId}`).then(response => response.data);
 }
 
 export const follow = (userId) => {
-    return axios.post(baseUrl + `follow/${userId}`, {},
-        {
-            withCredentials: true,
-            headers: {
-                "API-KEY": "8cc572da-38e6-49ab-a5f6-ce1b84a913a5"
-            }
-        }).then(response => response.data);
+    return instance.post(`follow/${userId}`, {}).then(response => response.data);
 }
 
 export const getProfile = (profileId) => {
-    return axios.get(baseUrl + `profile/${profileId}`).then(response => response.data);
+    return instance.get(`profile/${profileId}`).then(response => response.data);
 }
